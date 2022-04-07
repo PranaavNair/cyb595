4e5e0c8f7e16c90a44e3309146ee2866f2451b46
Pranav <Pranav@DESKTOP-S1A74RE>
2022-04-06 22:41:46 -0700

    new

---
+++ cyb595/src/cyb595/abc.java
@@ -0,0 +1,12 @@
+package cyb595;
+
+public class abc {
+	
+	public String checkSign(int number) {
+        if(number >= 0 ) {
+            return "positive";
+        } else {
+            return "negative";
+        }
+    }
+}
